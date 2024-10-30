0430651644813cef215f53057fb0f054af2ba12b
frknklc.632008 <frknklc.632008@gmail.com>
2024-10-30 11:32:14 +0000

    proje oluştu

---
+++ lib/const/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color lightScaffoldColor= Colors.white;
+  static const Color lightPrimary=Color(0xFF181059);
+  static const Color lightCardColor=Color.fromARGB(105,255,9,35);
+  static const Color darkScaffoldColor=Color.fromARGB(300,9,85,76); 
+  static const Color darkPrimary=Color.fromARGB(280,77,23,14);
+}
